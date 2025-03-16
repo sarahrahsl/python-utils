fix SA calculation error if empty mask

utils_python/feature_extractor.py
@@ -467,10 +467,16 @@ def calculate_gland_properties(tiff_path, niigz_path, z_levels, sliceno=None):
 
 
 def calculate_surface_area(mask):
+    from skimage.measure import marching_cubes
     """
     Compute the surface area of a 3D binary mask using the marching cubes algorithm.
     """
-    verts, faces, _, _ = marching_cubes(mask, level=0)  # Extract 3D isosurface
+    if not np.any(mask):  # Check if the mask is empty
+        return 0  
+
+    mask = (mask > 0).astype(np.uint8)  # Ensure binary mask
+
+    verts, faces, _, _ = marching_cubes(mask, level=0.5)  # Use level=0.5
     surface_area = np.sum(np.linalg.norm(np.cross(verts[faces[:, 1]] - verts[faces[:, 0]],
                                                   verts[faces[:, 2]] - verts[faces[:, 0]]), axis=1)) / 2
     return surface_area